2dc3407c304fa237e7ca0e47eee959055c96bede
Kent Fletcher <kent.fletcher@gmail.com>
2018-08-19 20:24:10 -0500

    add comments

--- src/main/java/com/fletcher/TriangleMaxSum.java
+++ src/main/java/com/fletcher/TriangleMaxSum.java
@@ -10,12 +10,15 @@ import java.util.Scanner;
  * By starting at the top of the triangle and moving to adjacent numbers on the
  * row below, the maximum total from top to bottom is 27.
  * 
- *       5
- *      9 6
- *     4 6 8
- *    0 7 1 5
- *    
- *    i.e. 5 + 9 + 6 + 7 = 27.
+<pre>
+				"5" + 
+				"9 6" + 
+				"4 6 8" + 
+				"0 7 1 5" ;
+
+</pre>
+ * 
+ * i.e. 5 + 9 + 6 + 7 = 27.
  * 
  * Write a program in Java to find the maximum total from top to bottom in the
  * included file ‘triangle.txt’ - a text file containing a triangle with 100
@@ -28,7 +31,7 @@ public class TriangleMaxSum
 {
 
 	/**
-	 * Main entry point. To execute on command line 
+	 * Main entry point. To execute on command line
 	 * 
 	 * > java -jar triangle-max-sum-1.0.jar < triangle.txt
 	 * 
@@ -46,6 +49,12 @@ public class TriangleMaxSum
 		computeTriangleMaxSum(triangleData);
 	}
 
+	/**
+	 * Read input file and return String[] where each element is line of input
+	 * 
+	 * @return String [] each element is line of input
+	 * @throws FileNotFoundException
+	 */
 	public static String[] readFile() throws FileNotFoundException
 	{
 		final Scanner scanner = new Scanner(System.in);
@@ -62,6 +71,13 @@ public class TriangleMaxSum
 		return sb.toString().split("###");
 	}
 
+	/**
+	 * Pass in String [] input where each element is a line of input and return a
+	 * List<List<Integer>>
+	 * 
+	 * @param fileData String[] input where each element is line of input
+	 * @return List<List<Integer>> data structure
+	 */
 	public static List<List<Integer>> toDataStruct(String[] fileData)
 	{
 
@@ -84,19 +100,31 @@ public class TriangleMaxSum
 		return tdl;
 	}
 
+	/**
+	 * Print to System.out the data structure represented by List<List<Integer>>
+	 * 
+	 * @param tdl   List<List<Integer>> input data structure
+	 * @param width for formatting purposes the width of each field
+	 */
 	public static void printTriangleData(List<List<Integer>> tdl, int width)
 	{
 		for (int i = 0; i < tdl.size(); i++)
 		{
 			for (int j = 0; j <= i; j++)
 			{
-				System.out.printf("%0"+width+"d ", tdl.get(i).get(j));
+				System.out.printf("%0" + width + "d ", tdl.get(i).get(j));
 			}
 			System.out.println();
 		}
 
 	}
 
+	/**
+	 * Compute the triangle max sum given the input data structure
+	 * 
+	 * @param tdl List<List<Integer>> input data structure
+	 * @return int the max of triangle sum
+	 */
 	public static int computeTriangleMaxSum(List<List<Integer>> tdl)
 	{
 
